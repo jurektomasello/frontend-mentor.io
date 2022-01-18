7bcc0870da3176bfef4212600484ca37753b4f7b
Jurek <jurek.tomasello@gmail.com>
2022-01-18 17:32:16 +0100

    Write function that checks if entered mail is valid, if not show error message

--- Base apparel coming soon master/index.js
+++ Base apparel coming soon master/index.js
@@ -1,11 +1,13 @@
-/*document.getElementById('send-button').onclick = */ 
-
 function mailCheck(inputText) {
     var mailForm = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if (inputText.match(mailForm)) {
-        return true;
+        document.getElementById('email-form').style.borderColor = '';
+        document.getElementById('invalid-mail-error').style.display = 'none';
+        document.getElementById('error-icon').style.display = 'none';
     } else {
-    document.getElementById('invalid-mail-error').style.display = 'block';
+        document.getElementById('email-form').style.borderColor = '#e99190';
+        document.getElementById('invalid-mail-error').style.display = 'block';
+        document.getElementById('error-icon').style.display = 'block';
     }
 }
